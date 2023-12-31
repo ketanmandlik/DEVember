39c3f3194e88fc46fdac49955e8f91e87ef9934d
ketanmandlik <ketanmandlik96@gmail.com>
2023-12-31 15:10:26 +0530

    Provide #Tag

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to DEVember</Text>
+      <Text>Welcome to #DEVember</Text>
       <StatusBar style="auto" />
     </View>
   );
